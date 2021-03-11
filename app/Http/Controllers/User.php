d9718178a2d8a4c58bce5ffeb7724c68e4688cbb
td-drive <aa.me.soara@outlook.jp>
2021-03-11 20:58:12 +0900

    mitei

---
+++ app/Http/Controllers/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models;
+
+class User extends Controller
+{
+    public function signup()
+    {
+        
+    }
+}
