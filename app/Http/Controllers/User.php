c623759331ef9e6531ab044d4a50767a6a0bc4e5
td-drive <aa.me.soara@outlook.jp>
2021-04-02 18:34:58 +0900

    新規登録フォームを追加

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models;
+use App\Models\User_md;
 
 class User extends Controller
 {
     public function signup()
     {
+        #Userモデルをインスタンス化
+        $md = new User_md;
+
+        #ユーザー名とパスワードをPOSTメソッドで取得
+        $name = $_POST['name'];
+        $passwd = $_POST['passwd'];
+        $mail = $_POST['mail'];
+
+        password_hash($passwd,PASSWORD_DEFAULT);
         
+            $md->fill(['name'=>$name,'password'=>$passwd,'mail'=>$mail]);
+            $insert = $md->save();
+            
+                session();
+
+        return view('signup_true',compact('insert'));
     }
+
 }
